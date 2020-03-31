e4a64dcd656edba72d8371621ba9637282204640
abhinav <bhattabhinav@github.com>
2020-03-31 17:04:22 -0400

    initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, 1.0f * Time.deltaTime);
+
+        //These are player inputs 
+        if (Input.GetKey(KeyCode.A)) transform.Rotate(0, -45 * Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.D)) transform.Rotate(0, 45 * Time.deltaTime, 0);
+
+    }
+}
